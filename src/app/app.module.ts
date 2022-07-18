bfd6777b36360c753b0eea4b77a4a8f78457e9ba
andresaluz <103433730+andresaluz@users.noreply.github.com>
2022-07-18 10:58:59 -0300

    Movimentacao List
    Movimentacao List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
+import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovimentacaoListComponent
+    MovimentacaoListComponent,
+    MovimentacaoNewComponent
   ],
   imports: [
     BrowserModule,
